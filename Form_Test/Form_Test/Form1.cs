dc015936fbd75e1d7f1634ce6a12c21414dcd747
Mona Lisa <keitokei1027@gmail.com>
2025-10-24 15:36:44 +0900

    これはC#のフォームより作成したものです。

---
+++ Form_Test/Form_Test/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    //インスタンスの生成
+                    TestBotton testBotton = new TestBotton();
+
+                    //ボタンの位置を設定
+                    testBotton.Location = new Point(50 * i, 50 * j);
+
+                    //ボタンの大きさを設定
+                    testBotton.Size = new Size(50, 50);
+
+                    //ボタン内のテキストを設定
+                    testBotton.Text = "絶対に押すな";
+
+                    //コントロールにボタンを追加
+                    Controls.Add(testBotton);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ようこそ！C#の世界へ！");
+        }
+    }
+}
---
+++ Form_Test/Form_Test/TestBotton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Test
+{
+    //Bottonクラスを継承したTestButton
+    internal class TestBotton : Button
+    {
+
+    }
+
+}
